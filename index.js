41162fe15179b8797a4508dfa1b6e6d410846ed1
Dakevo31 <kevisali.24@gmail.com>
2025-07-16 12:51:26 -0600

    Fix: removed require and using env only

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ require("dotenv").config();
 
 // Inicializar Firebase Admin
 const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
+// ✅ Corregir saltos de línea
+serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
